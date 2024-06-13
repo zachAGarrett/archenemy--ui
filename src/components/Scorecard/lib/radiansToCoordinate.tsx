f1eef3aa2a395edf33179da81e99600070c7d39e
Zach Garrett <zach@ketari.io>
2024-06-12 23:42:49 -0400

    moved files around, rename, refactor

--- src/components/Scorecard/lib/radiansToCoordinate.tsx
+++
@@ -1,7 +0,0 @@
-import { CoordinatePair } from "./types";
-
-export default function radiansToCoordinate(radians: number): CoordinatePair {
-  const x = Math.cos(radians);
-  const y = Math.sin(radians);
-  return [x, y];
-}
--- src/components/Scorecard/lib/radiansToDegrees.tsx
+++
@@ -1,5 +0,0 @@
-export default function radiansToDegrees(radians: number) {
-  const r = (radians * 180) / Math.PI;
-  if (r < 0) return 360 + r;
-  return r;
-}
